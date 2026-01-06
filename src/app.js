3ec02ad8f83b3009530d91b9ced05cd52dbc9142
amruthakvb <venkata.korumilli@tcs.com>
2026-01-06 08:15:33 +0000

    app file changes

--- src/app.js
+++ src/app.js
@@ -7,34 +7,38 @@ import { seedAuthenticationTypes } from "./seed/authenticationTypes.seed.js";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from './routes/user.routes.js'
 import roleRoutes from './routes/role.routes.js'
+import authTypeRoutes from './routes/authenticationType.routes.js'
+// import masterDataRoutes from './routes/masterData.routes.js'
 dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin:'http://localhost:5173',
-  methods:['GET','POST','PUT','PATCH','DELETE'],
-  allowedHeaders:['Content-type','Authorization'],
-  credentials:true
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-type', 'Authorization'],
+  credentials: true
 }
 ));
 app.use(express.json());
 app.get("/health", (req, res) => {
- res.json({ status: "OK" });
+  res.json({ status: "OK" });
 });
 app.use("/auth", authRoutes);
 // test DB connection & sync tables once at startup
 (async () => {
- try {
-   await db.sequelize.authenticate();
-   console.log("✅ Database connection established");
-   await db.sequelize.sync({ alter: true });
-   console.log("✅ Models synchronized");
-   await seedAuthenticationTypes();
- } catch (error) {
-   console.error("❌ DB connection error:", error);
- }
+  try {
+    await db.sequelize.authenticate();
+    console.log("✅ Database connection established");
+    await db.sequelize.sync({ alter: true });
+    console.log("✅ Models synchronized");
+    await seedAuthenticationTypes();
+  } catch (error) {
+    console.error("❌ DB connection error:", error);
+  }
 })();
 
 app.use("/api/users", userRoutes);
-app.use("/api/roles",roleRoutes);
+app.use("/api/roles", roleRoutes);
+app.use('/api/authType', authTypeRoutes);
+// app.use('/api/masterDataSync', masterDataRoutes);
 export default app;
