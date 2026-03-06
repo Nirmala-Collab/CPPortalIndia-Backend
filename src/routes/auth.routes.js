73c35283a88f12f47680aabfdf20d36a6a204bba
amruthakvb <venkata.korumilli@tcs.com>
2026-03-06 13:01:13 +0000

    rate limiting

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,11 +3,12 @@ import express from 'express';
 import { login, sendOtp, verifyOtp, logout } from '../controllers/auth.controller.js';
 import { refreshAccessToken } from '../controllers/refreshToken.controller.js';
 import { otpLimiter } from '../middleware/otpLimiter.memory.js';
+import { otpVerifyLimiter } from '../middleware/otpVerifyLimiter.memory.js';
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/send-otp', express.json(), otpLimiter, sendOtp);
-router.post('/verify-otp', verifyOtp);
+router.post('/verify-otp', express.json(), otpVerifyLimiter, verifyOtp);
 router.post('/token/refresh', refreshAccessToken);
 router.post('/logout', logout);
 export default router;
